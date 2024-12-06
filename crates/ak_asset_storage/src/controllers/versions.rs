6792b9ce568b0a72d67feb92f2f8e83595f4ede3
夕舞八弦 <1677759063@qq.com>
2024-12-07 00:17:07 +0800

    feat: version list接口不响应hot update list

--- crates/ak_asset_storage/src/controllers/versions.rs
+++ crates/ak_asset_storage/src/controllers/versions.rs
@@ -5,7 +5,7 @@ use crate::{
         _entities::versions::{Entity, Model},
         bundles,
     },
-    views::{utils::json, FileDetail},
+    views::{utils::json, versions::VersionListItem, FileDetail},
 };
 use axum::{
     debug_handler,
@@ -18,21 +18,17 @@ use utoipa::IntoParams;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
 #[debug_handler]
-#[utoipa::path(get, path = "",tag = "version", responses((status = OK, body = [Model])))]
+#[utoipa::path(get, path = "",tag = "version", responses((status = OK, body = [VersionListItem])))]
 pub async fn list(State(ctx): State<Context>) -> Result<Response> {
     let resp = Entity::find().all(&ctx.conn).await?;
     Ok(json(resp))
 }
 
-async fn load_item(ctx: &Context, id: i32) -> Result<Model> {
-    let item = Entity::find_by_id(id).one(&ctx.conn).await?;
-    item.ok_or_else(|| Error::NotFound)
-}
-
 #[debug_handler]
 #[utoipa::path(get, path = "/{id}",tag = "version", responses((status = OK, body = Model)))]
 pub async fn get(Path(id): Path<i32>, State(ctx): State<Context>) -> Result<Response> {
-    load_item(&ctx, id).await.map(json)
+    let item = Entity::find_by_id(id).one(&ctx.conn).await?;
+    item.ok_or_else(|| Error::NotFound).map(json)
 }
 
 #[debug_handler]
